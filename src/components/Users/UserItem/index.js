85be08e72e231d0407d9bea85e3b976d30593261
ferreiraz <ferreiraz@pop-os.localdomain>
2021-10-23 11:12:30 -0300

    update user data

--- src/components/Users/UserItem/index.js
+++ src/components/Users/UserItem/index.js
@@ -1,11 +1,20 @@
 
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
 
 import { withFirebase } from "../../../firebase"
 
 function UserItem(props){
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState()
+
+  const formInititalValues = {
+    username: '',
+    email: '',
+  };
+
+  const [formValues, setFormValues] = useState(formInititalValues)
+  const { username, email } = formValues;
 
   useEffect(() => {
     props.firebase.user(props.match.params.id).on('value', snapshot => {
@@ -20,7 +29,28 @@ function UserItem(props){
     props.firebase.db.ref(`users/${uid}`).remove()
   }
 
-  console.log(user)
+  let currentId = useParams();
+  const { id } = currentId
+  console.log(id)
+
+  const onSubmit = (e) => {
+    props.firebase.db.ref(`users/${id}`).set(formValues).then(() => {
+      setFormValues({...formValues})
+      props.history.push('/admin')
+    }).catch(error => {
+      console.log(error)
+    })
+
+    e.preventDefault();
+  }
+
+  const onChange = (e) => {
+    let { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
 
   return (
     <div>
@@ -32,20 +62,29 @@ function UserItem(props){
               <span>
                 <strong>ID:</strong> {user.uid}
               </span>
-              <span>
-                <strong>E-Mail:</strong> {user.email}
-              </span>
-              <span>
-                <strong>Username:</strong> {user.username}
-              </span>
-              <span>
-                <button
-                  type="button"
-                  
-                >
-                  Send Password Reset
-                </button>
-              </span>
+              <form onSubmit={onSubmit}>
+                <div>
+                  <strong>Nome:</strong> {user.username}
+                  <input
+                    name="username"
+                    value={username}
+                    onChange={onChange}
+                    type="text"
+                    placeholder={user.username}
+                  />
+                </div>
+                <div>
+                  <strong>E-Mail:</strong> {user.email}
+                  <input 
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                    type="text"
+                    placeholder={user.email}
+                  />
+                </div>
+                <button type="submit">Atualizar Dados</button>
+              </form>
             </div>
           )}
         </ul>
